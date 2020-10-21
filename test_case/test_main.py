462d84c400ec6f4808cb1b7fcb9d8641ef076307
黄赐金 <531405546@qq.com>
2020-10-21 16:58:08 +0800

    第四次提交

--- test_case/test_main.py
+++ test_case/test_main.py
@@ -3,6 +3,10 @@
 # author:huangCijin
 # datetime:2020/10/21 15:24
 # software: PyCharm
-print('hcj你好帅哦！')
-print('njnjnjnjj')
-print("艰苦艰苦艰苦看见就")
+class Test_HCJ:
+    def test_001(self):
+        print('hcj你好帅哦！')
+    def test_002(self):
+        print('njnjnjnjj')
+    def test_0003(self):
+        print("艰苦艰苦艰苦看见就")
