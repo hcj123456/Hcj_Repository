86877edd7e5bd693d4a0b5cbe47f5b58e5e83288
黄赐金 <531405546@qq.com>
2020-10-22 16:25:08 +0800

    第十一次提交

--- test_case/test_main.py
+++ test_case/test_main.py
@@ -8,16 +8,15 @@ import pytest
 
 
 class Test_HCJ:
-    @allure.step(title='第一次测试')
-    def test_001(self):
-        mkmk
-        allure.attach("输出好帅","11111")
+    #@allure.step(title='第一次测试')
+    def test_001():
+        #allure.attach("输出好帅","11111")
         print('hcj你好帅哦！')
-    @allure.step(title='第二次测试')
-    @pytest.allure.testcase("aaaaa")
+    #@allure.step(title='第二次测试')
+    #@allure.testcase("aaaaa")
     def test_002(self):
         print('njnjnjnjj')
-    @allure.step(title='第三次测试')
-    @allure.issue("ssssss","查看bug")
+    ##@allure.step(title='第三次测试')
+    #@allure.issue("ssssss","查看bug")
     def test_0003(self):
         print("艰苦艰苦艰苦看见就")
